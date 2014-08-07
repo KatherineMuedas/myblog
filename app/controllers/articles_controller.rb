da55f70e444d4a5ae15b05a7468c8eb7667ea6f9
katherine <muedasnka@hmail.com>
2014-08-07 11:33:21 -0500

    add security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
-
-def index
-  @articles = Article.all
-end
+ 
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
+ 
+  # snipped for brevity
 def new
   @article = Article.new
 end
